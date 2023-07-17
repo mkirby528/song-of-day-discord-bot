e00d0ab9a5f94515acbcb25dab9e8611a3b3ff36
Matthew Kirby <mkirby528@Matthews-MacBook-Air.local>
2023-07-17 16:34:40 -0400

    add dynamo for dedup

--- src/spotify.py
+++ src/spotify.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
 import random
-
+from src.dynamo import insert_item, item_in_table
 
 auth_manager = SpotifyClientCredentials()
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
@@ -14,11 +14,19 @@ playlists = ["62QmFVktcgA00V8q4QnxSH"]
 def get_random_song():
     playlist_items = get_playlist_tracks(random.choice(playlists))
     track = random.choice(playlist_items)["track"]
-    return ({
-            'name': track['name'],
-            'artist': ", ".join(_get_artists(track)),
-            'url': track["external_urls"]["spotify"]
-            })
+    count = 0
+    while item_in_table(track) and count < len(playlist_items):
+        print("Getting random song from playlist....")
+        track = random.choice(playlist_items)["track"]
+        count += 1
+    formatted_song = {
+        'id': track['id'],
+        'name': track['name'],
+        'artist': ", ".join(_get_artists(track)),
+        'url': track["external_urls"]["spotify"]
+    }
+    insert_item(formatted_song)
+    return formatted_song
 
 
 def get_playlist_tracks(playlist_id):
